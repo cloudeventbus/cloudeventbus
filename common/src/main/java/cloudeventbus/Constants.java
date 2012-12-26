6e99786b19e3864147ee8bf59cb1971781f43699
Mike Heath <elcapo@gmail.com>
2012-12-26 11:43:22 -0700

    Common constants used across Cloud Event Bus.

--- common/src/main/java/cloudeventbus/Constants.java
+++ common/src/main/java/cloudeventbus/Constants.java
@@ -4,4 +4,14 @@ package cloudeventbus;
  * @author Mike Heath <heathma@ldschurch.org>
  */
 public class Constants {
+
+	/**
+	 * 4k default maximum message size.
+	 */
+	public static final int DEFAULT_MAX_MESSAGE_SIZE = 4096;
+
+	private Constants() {
+		// Don't instantiate this class.
+	}
+
 }
