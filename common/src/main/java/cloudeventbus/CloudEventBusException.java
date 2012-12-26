8e70a7835f6ff82e4953e6611c67e58fd74b14f9
Mike Heath <elcapo@gmail.com>
2012-12-26 11:39:50 -0700

    Added root exception for all cloud event bus exceptions.

--- common/src/main/java/cloudeventbus/CloudEventBusException.java
+++ common/src/main/java/cloudeventbus/CloudEventBusException.java
@@ -3,5 +3,12 @@ package cloudeventbus;
 /**
  * @author Mike Heath <heathma@ldschurch.org>
  */
-public class CloudEventBusException {
+public class CloudEventBusException extends RuntimeException {
+	public CloudEventBusException(String message) {
+		super(message);
+	}
+
+	public CloudEventBusException(String message, Throwable cause) {
+		super(message, cause);
+	}
 }
