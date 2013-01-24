8af3ba3335487066e2fd7be1a3ac7eb5c00b576e
Mike Heath <elcapo@gmail.com>
2013-01-23 22:37:19 -0700

    Implemented initial publishing CLI client.

--- cli/src/main/java/cloudeventbus/cli/ClientPublish.java
+++ cli/src/main/java/cloudeventbus/cli/ClientPublish.java
@@ -16,68 +16,64 @@
  */
 package cloudeventbus.cli;
 
-import cloudeventbus.pki.CertificateChain;
+import cloudeventbus.client.ConnectionStateListener;
+import cloudeventbus.client.Connector;
+import cloudeventbus.client.EventBus;
 import cloudeventbus.pki.CertificateUtils;
 import cloudeventbus.pki.TrustStore;
-import cloudeventbus.server.ServerChannelInitializer;
 import com.beust.jcommander.JCommander;
 import com.beust.jcommander.Parameter;
 import com.beust.jcommander.ParameterException;
-import io.netty.bootstrap.ServerBootstrap;
-import io.netty.channel.socket.nio.NioEventLoopGroup;
-import io.netty.channel.socket.nio.NioServerSocketChannel;
 
-import java.net.InetSocketAddress;
-import java.security.PrivateKey;
 import java.util.List;
 
 /**
  * @author Mike Heath <elcapo@gmail.com>
  */
-public class Server {
+public class ClientPublish {
+
 	public static void main(String[] args) throws Exception {
 		final JCommander commander = new JCommander();
 
 		final Options options = new Options();
 		commander.addObject(options);
 
-		commander.setProgramName("eventbus-server");
+		commander.setProgramName("eventbus-publish");
 
 		try {
 			commander.parse(args);
 
-			TrustStore trustStore = CertificateUtils.loadTrustStore(options.trustStore);
-			if (trustStore != null && trustStore.size() > 0) {
-				System.out.println("Using trust store at: " + options.trustStore);
-			} else {
-				trustStore = null;
+			if (options.mainParameters == null || options.mainParameters.size() != 2) {
+				commander.usage();
+				System.exit(1);
 			}
 
-			final int port = options.port;
+			final Connector connector = new Connector();
+			final TrustStore trustStore = CertificateUtils.loadTrustStore(options.trustStore);
+			if (trustStore != null && trustStore.size() > 0) {
+				System.out.println("Using trust store at: " + options.trustStore);
+				connector.trustStore(trustStore);
+			}
 
-			final CertificateChain certificateChain;
-			final PrivateKey privateKey;
-			if (options.certificate == null) {
-				certificateChain = null;
-				privateKey = null;
-			} else {
-				certificateChain = CertificateUtils.loadCertificateChain(options.certificate);
+			connector.addServer(options.host, options.port);
+			if (options.certificate != null) {
+				connector.certificateChain(CertificateUtils.loadCertificateChain(options.certificate));
 				if (options.privateKey == null) {
 					System.err.print("You must specify a private key when using a certificate.");
 					System.exit(1);
 				} else {
-					privateKey = CertificateUtils.loadPrivateKey(options.privateKey);
+					connector.privateKey(CertificateUtils.loadPrivateKey(options.privateKey));
 				}
 			}
-			// TODO Add support for specifying certificate and private key
-			// TODO Implement clustering
-			new ServerBootstrap()
-					.group(new NioEventLoopGroup(), new NioEventLoopGroup())
-					.channel(NioServerSocketChannel.class)
-					.localAddress(new InetSocketAddress(port))
-					.childHandler(new ServerChannelInitializer("eventbus-simple-server", trustStore))
-					.bind().awaitUninterruptibly();
-			System.out.println("Server listening on port " + port);
+			connector.addConnectionStateListener(new ConnectionStateListener() {
+				@Override
+				public void onConnectionStateChange(EventBus eventBus, State state) {
+					if (state == State.SERVERY_READY) {
+						eventBus.publish(options.mainParameters.get(0), options.mainParameters.get(1));
+						eventBus.close();
+					}
+				}
+			}).connect();
 		} catch (ParameterException e) {
 			System.err.println(e.getMessage());
 			commander.usage();
@@ -87,16 +83,21 @@ public class Server {
 
 	private static class Options extends DefaultOptions {
 
+		@Parameter(description = "subject body")
+		List<String> mainParameters;
+
+		@Parameter(names = "-host", description = "Ths host the server is running on.")
+		String host = "localhost";
+
 		@Parameter(names = "-port", description = "The port the server will listen on.")
 		int port = 4223;
 
-		@Parameter(names = "-certificate", description = "The file containing the certificate used to identify the server.")
+		@Parameter(names = "-certificate", description = "The file containing the certificate used to identify the client.")
 		String certificate;
 
-		@Parameter(names = "-privateKey", description = "The file containing the private key for the server's certificate.")
+		@Parameter(names = "-privateKey", description = "The file containing the private key for the client's certificate.")
 		String privateKey;
 
-		@Parameter(names = "-peer", description = "A peer server to cluster with (e.g. -peer 10.1.2.3:4321)")
-		List<String> peers;
 	}
+
 }
